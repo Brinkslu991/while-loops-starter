3b6991a6b97a39b984aec410f6f5507b01da57cc
Manfred Spitze <155758012+manfredspitze@users.noreply.github.com>
2024-11-17 07:21:29 -0500

    Delete mult-table-input-example.py

--- mult-table-input-example.py
+++
@@ -1,5 +0,0 @@
-factor = int(input('What number do you want to generate a times table for? (Example: 6)\n'))
-
-for counter in range(5, 11, 2):
-    result = counter * factor
-    print(f'{counter} times {factor} = {result}')
